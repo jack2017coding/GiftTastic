02f4a32f706941453845c376c64a593f3323c8ab
harbasheng <jack.bashirian@harbash.com>
2017-06-05 20:45:41 -0400

    rev 3

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -32,7 +32,7 @@ $(document).on("click", "button", function() {
       var birdI = $(this).attr("data-name");
 
       // Constructing a queryURL using the animal name
-      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         birdI + "&api_key=dc6zaTOxFJmzC&limit=10";
 
       console.log(queryURL);
